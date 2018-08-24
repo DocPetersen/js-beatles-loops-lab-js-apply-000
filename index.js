6795fb3984533802aa2cb4cca3c6f54a392afb6d
REESE PETERSEN <reese.r.petersen@gmail.com>
2018-08-24 21:54:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function theBeatlesPlay(musicians, instruments) {
   var array = [];
   for (var i = 0; i < muscians.length; i++) {
-  array.push(musicians) + 'plays' + (instruments[i];
+    array.push(musicians) + 'plays' + (instruments[i];
 }
 return array;
 }
